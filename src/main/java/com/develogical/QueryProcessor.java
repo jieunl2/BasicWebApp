cb9aa1c5ac173584b6bcf823ea1f0b1d66e82934
Jieun Lee <jieunl2@andrew.cmu.edu>
2022-09-22 12:33:09 -0400

    Breaking build

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -14,6 +14,7 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("17313")) {
             return "Foundations of Software Engineering is offered at CMU";
         }
+        if (query.toLowerCase().contains("break")) 
         return "";
     }
 }
